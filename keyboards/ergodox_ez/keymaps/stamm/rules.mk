0542391db41c9d32baf36550c823493a9ca2acf3
Rustam Zagirov <stammru@gmail.com>
2021-04-01 13:55:13 +0300

    add more variables

--- keyboards/ergodox_ez/keymaps/stamm/rules.mk
+++ keyboards/ergodox_ez/keymaps/stamm/rules.mk
@@ -1,8 +1,12 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LTO_ENABLE = yes
+EXTRAFLAGS += -flto
 COMMAND_ENABLE = no
 LEADER_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 
 TAP_DANCE_ENABLE = no
+# UNICODE_ENABLE = no
+MIDI_ENABLE = no
+# DYNAMIC_MACRO_ENABLE = no
